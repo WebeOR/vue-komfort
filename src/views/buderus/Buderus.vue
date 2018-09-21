5153f85ce430c3fe450fcaa1b6860597cf054ba6
lisavvveta <https://github.com/lisavvveta>
2018-09-21 20:18:02 +0700

    Подключение стилей в buderus

---
+++ src/views/buderus/Buderus.vue
@@ -0,0 +1,249 @@
+<template>
+
+  <main id="main" class="main">
+
+    <section class="intro">
+      <div class="intro__back-image-container">
+        <img src="assets/img/buderus/intro.jpg" alt="Buderus intro" class="intro__back-image">
+      </div>
+      <div class="intro__overlay">
+        <div class="container_flex">
+          <h1 class="intro__title">Твёрдотопливные котлы Buderus</h1>
+          <span class="intro__subtitle">Выберите интересующий Вас вариант, и свяжитесь с менеджером, чтобы уточнить наличие и стоимость:</span>
+
+          <ul class="intro__list">
+            <li><a href="#" data-roll="s111" class="intro__link" ripple-light>Logano S111</a></li>
+            <li><a href="#" data-roll="s131" class="intro__link" ripple-light>Logano S131</a></li>
+          </ul>
+
+          <span class="intro__additional-text">Котельная на твёрдом топливе может использоваться даже там, куда не подведены ни газопровод, ни линия электропередач, - твердотопливные котлы полностью автономны. Это преимущество твердотопливных отопительных котлов в сочетании с невысокой стоимостью котла, а также доступностью и дешевизной топлива, обусловливает их популярность в России</span>
+          <span class="intro__list-title">Другие семейства твёрдотопливных котлов:</span>
+
+          <ul class="intro__list">
+            <li><a href="kentatsu" class="intro__link" ripple-light>Kentatsu Furst Vulkan</a></li>
+            <li><a href="termokraft" class="intro__link" ripple-light>Termokraft</a></li>
+            <li><a href="zota" class="intro__link" ripple-light>Zota</a></li>
+            <li><a href="nmk" class="intro__link" ripple-light>НМК</a></li>
+            <li><a href="energy-tt" class="intro__link" ripple-light>Энергия-ТТ</a></li>
+          </ul>
+
+        </div>
+      </div>
+    </section>
+
+
+    <section id="s111" class="product">
+      <div class="product__container">
+        <h2 class="product__name">Buderus </h2>
+        <div class="container_flex-row">
+          <div class="product__image-container">
+            <div class="carousel-cell">
+              <img src="assets/img/buderus/111.jpg" alt="Buderus Logano S111-2" class="product__image">
+            </div>
+          </div>
+
+          <div class="product__description-container">
+            <span class="product__characteristics-title">Характеристики:</span>
+            <ul class="product__characteristics-list">
+              <li class="product__characteristics-item">
+                Котел BUDERUS LOGANO S111-2 мощностью до 28 кВт – представитель обновленной линейки котлов Buderus Logano S111;
+              </li>
+              <li class="product__characteristics-item">
+                Как и в предыдущей версии, котел разработан для простого, приятного и эффективного отопления, LOGANO S111-2 сжигает древесину, бурый и каменный уголь, кокс и прессованное сорта твердого топлива. 8 типоразмеров серии от 12 до 45 кВт позволяют выбрать наиболее подходящий вариант котла. Отличительная особенность серии S111-2 – усовершенствованный внешний вид котла;
+              </li>
+              <li class="product__characteristics-item">
+                Теперь котел LOGANO S111-2 – это стильный и элегантный котел с модифицированной конструкцией для современного отопления;
+              </li>
+              <li class="product__characteristics-item">
+                Конструктивные изменения котла коснулись некоторых других деталей оборудования: для удобства и быстрого монтажа фланцевые подключения заменены на резьбовые, усовершенствованы крепления изоляции и петель зольной дверцы котла. Узкие габаритные размеры котла позволяют использовать его в небольших помещениях под котельную.
+              </li>
+            </ul>
+          </div>
+
+          <div class="product__table-container" align="center">
+            <table style="border-collapse:collapse;table-layout:fixed" width="840" border="0" cellspacing="0" cellpadding="0">
+              <colgroup>
+                <col width="311" />
+                <col width="139" />
+                <col width="126" />
+                <col width="136" />
+                <col width="128" />
+              </colgroup>
+              <tbody>
+                <tr height="40">
+                  <td>Характеристики\Наименование</td>
+                  <td width="139">Logano S111-2 12</td>
+                  <td width="126">Logano S111-2 20</td>
+                  <td width="136">Logano S111-2 24</td>
+                  <td width="128">Logano S111-2 32</td>
+                </tr>
+                <tr height="20">
+                  <td>Мощность, кВт</td>
+                  <td>13,5</td>
+                  <td>20</td>
+                  <td>24</td>
+                  <td>32</td>
+                </tr>
+                <tr height="20">
+                  <td>Отапливаемая площадь, м²</td>
+                  <td>135</td>
+                  <td>200</td>
+                  <td>240</td>
+                  <td>320</td>
+                </tr>
+                <tr height="20">
+                  <td>Объем топки, л</td>
+                  <td>26</td>
+                  <td>46</td>
+                  <td>46</td>
+                  <td>61</td>
+                </tr>
+                <tr height="20">
+                  <td>Объем теплоносителя, л</td>
+                  <td>46</td>
+                  <td>56</td>
+                  <td>57</td>
+                  <td>64</td>
+                </tr>
+                <tr height="20">
+                  <td>Минимальная температура теплоносителя, ˚С</td>
+                  <td>65</td>
+                  <td>65</td>
+                  <td>65</td>
+                  <td>65</td>
+                </tr>
+                <tr height="20">
+                  <td>Рабочая температура теплоносителя, ˚С</td>
+                  <td>95</td>
+                  <td>95</td>
+                  <td>95</td>
+                  <td>95</td>
+                </tr>
+                <tr height="20">
+                  <td>Рабочее давление, Бар</td>
+                  <td>2,5</td>
+                  <td>2,5</td>
+                  <td>2,5</td>
+                  <td>2,5</td>
+                </tr>
+                <tr height="20">
+                  <td>Диаметр соединения, мм</td>
+                  <td>50</td>
+                  <td>40</td>
+                  <td>40</td>
+                  <td>40</td>
+                </tr>
+                <tr height="20">
+                  <td>Диаметр дымохода, мм</td>
+                  <td>150</td>
+                  <td>150</td>
+                  <td>150</td>
+                  <td>150</td>
+                </tr>
+                <tr height="20">
+                  <td>Высота дымохода, м</td>
+                  <td>6</td>
+                  <td>10</td>
+                  <td>10</td>
+                  <td>10</td>
+                </tr>
+                <tr height="20">
+                  <td>Вид топлива</td>
+                  <td>дрова, уголь</td>
+                  <td>дрова, уголь</td>
+                  <td>дрова, уголь</td>
+                  <td>дрова, уголь</td>
+                </tr>
+                <tr height="20">
+                  <td>Высота, мм</td>
+                  <td>600</td>
+                  <td>990</td>
+                  <td>990</td>
+                  <td>990</td>
+                </tr>
+                <tr height="20">
+                  <td>Ширина, мм</td>
+                  <td>730</td>
+                  <td>700</td>
+                  <td>700</td>
+                  <td>700</td>
+                </tr>
+                <tr height="20">
+                  <td>Глубина, мм</td>
+                  <td>920</td>
+                  <td>770</td>
+                  <td>770</td>
+                  <td>870</td>
+                </tr>
+                <tr height="20">
+                  <td>Масса не более, кг</td>
+                  <td>158</td>
+                  <td>200,50</td>
+                  <td>215</td>
+                  <td>240</td>
+                </tr>
+                <tr height="20">
+                  <td>Цена, руб</td>
+                  <td>44 150</td>
+                  <td>54 290</td>
+                  <td>61 825</td>
+                  <td>64 895</td>
+                </tr>
+              </tbody>
+            </table>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </section>
+
+
+    <div class="s-devider">
+      <div class="line"></div>
+    </div>
+
+    <S-131 />
+
+    <div class="s-devider">
+      <div class="line"></div>
+    </div>
+
+    <div class="families">
+      <span class="families__list-title">Другие семейства твёрдотопливных котлов:</span>
+      <ul class="families__list">
+        <li><a href="kentatsu.html" class="families__link" ripple-light>Kentatsu Furst Vulkan</a></li>
+        <li><a href="termokraft.html" class="families__link" ripple-light>Termokraft</a></li>
+        <li><a href="zota.html" class="families__link" ripple-light>Zota</a></li>
+        <li><a href="nmk.html" class="families__link" ripple-light>НМК</a></li>
+        <li><a href="energy-tt.html" class="families__link" ripple-light>Энергия-ТТ</a></li>
+      </ul>
+
+    </div>
+
+  </main>
+</template>
+
+<script>
+
+  import s131 from './s131.vue';
+
+  export default {
+    name: 'Buderus',
+    components: { s131 }
+  };
+
+</script>
+
+<style lang="scss">
+    
+    import buderus from './stilesheets/sections/buderus.scss'
+    
+    export default {
+    name: 'Buderus',
+    components: { buderus}
+  };
+    
+</style>
