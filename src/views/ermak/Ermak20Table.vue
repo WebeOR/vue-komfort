5afdcc92166765a99cc38be2df3b288310061974
AlexQuidditch <alexquidditch@gmail.com>
2018-09-24 16:44:05 +0300

    - таблицы вынесены

---
+++ src/views/ermak/Ermak20Table.vue
@@ -0,0 +1,91 @@
+<template>
+  <table
+    class="product-table"
+    width="1064"
+    border="0"
+    cellspacing="0"
+    cellpadding="0">
+    <colgroup>
+      <col width="175">
+      <col width="116">
+      <col width="85">
+      <col width="64">
+      <col width="99">
+      <col width="85">
+      <col width="88">
+      <col width="74">
+      <col width="68">
+      <col width="77">
+      <col width="133">
+    </colgroup>
+    <tbody>
+      <tr height="20">
+        <td colspan="11">Печь-каменка Ермак 20</td>
+      </tr>
+      <tr height="43">
+        <td rowspan="2">Модель печи-каменки</td>
+        <td rowspan="2">Объём отапливаемого помещения, м³</td>
+        <td rowspan="2">Мощность, кВт</td>
+        <td rowspan="2">Масса, кг</td>
+        <td rowspan="2">Ёмкость каменки, кг</td>
+        <td rowspan="2">Глубина топки, мм</td>
+        <td rowspan="2">Диаметр дымохода, м</td>
+        <td colspan="3">Габаритные размеры, ±15 мм</td>
+        <td rowspan="2">Длина тоннеля, мм</td>
+      </tr>
+      <tr height="41">
+        <td>Длина</td>
+        <td>Ширина</td>
+        <td>Высота</td>
+      </tr>
+      <tr height="20">
+        <td>Ермак 20</td>
+        <td rowspan="4">12-24</td>
+        <td rowspan="4">20</td>
+        <td>70</td>
+        <td rowspan="4">60</td>
+        <td rowspan="4">550</td>
+        <td rowspan="4">115</td>
+        <td>700</td>
+        <td>445</td>
+        <td>760</td>
+        <td>200</td>
+      </tr>
+      <tr height="20">
+        <td>Ермак 20-ПС</td>
+        <td>75</td>
+        <td>700</td>
+        <td>515</td>
+        <td>760</td>
+        <td>200… 300</td>
+      </tr>
+      <tr height="20">
+        <td>Ермак-Элит 20-С</td>
+        <td>60</td>
+        <td>700</td>
+        <td>445</td>
+        <td>760</td>
+        <td>200</td>
+      </tr>
+      <tr height="20">
+        <td>Ермак-Элит 20-ПС</td>
+        <td>65</td>
+        <td>700</td>
+        <td>515</td>
+        <td>760</td>
+        <td>200… 300</td>
+      </tr>
+      <tr height="20">
+        <td colspan="11">*стоимость уточняйте у менеджера</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+
+  export default {
+    name: 'Ermak-20-Table'
+  };
+
+</script>
---
+++ src/views/ermak/Ermak30Table.vue
@@ -0,0 +1,73 @@
+<template>
+  <table
+    class="product-table"
+    width="1064"
+    border="0"
+    cellspacing="0"
+    cellpadding="0">
+    <colgroup>
+      <col width="175">
+      <col width="116">
+      <col width="85">
+      <col width="64">
+      <col width="99">
+      <col width="85">
+      <col width="88">
+      <col width="74">
+      <col width="68">
+      <col width="77">
+      <col width="133">
+    </colgroup>
+    <tbody>
+      <tr height="20">
+        <td colspan="11" height="20">Печь-каменка Ермак 30</td>
+      </tr>
+      <tr height="43">
+        <td rowspan="2">Модель печи-каменки</td>
+        <td rowspan="2">Объём отапливаемого помещения, м³</td>
+        <td rowspan="2">Мощность, кВт</td>
+        <td rowspan="2">Масса, кг</td>
+        <td rowspan="2">Ёмкость каменки, кг</td>
+        <td rowspan="2">Глубина топки, мм</td>
+        <td rowspan="2">Диаметр дымохода, м</td>
+        <td colspan="3">Габаритные размеры, ±15 мм</td>
+        <td rowspan="2">Длина тоннеля, мм</td>
+      </tr>
+      <tr height="41">
+        <td>Длина</td>
+        <td>Ширина</td>
+        <td>Высота</td>
+      </tr>
+      <tr height="20">
+        <td>Ермак 30-ПС/2К/РТ</td>
+        <td rowspan="2">15-30</td>
+        <td rowspan="2">28</td>
+        <td rowspan="2">100</td>
+        <td rowspan="2">40/25</td>
+        <td rowspan="2">550</td>
+        <td rowspan="2">130</td>
+        <td>700</td>
+        <td>600</td>
+        <td>900</td>
+        <td rowspan="2">160</td>
+      </tr>
+      <tr height="20">
+        <td>Ермак 30-2ПС/2К/РТ</td>
+        <td>700</td>
+        <td>600</td>
+        <td>900</td>
+      </tr>
+      <tr height="20">
+        <td colspan="11">*стоимость уточняйте у менеджера</td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+
+  export default {
+    name: 'Ermak-30-Table'
+  };
+
+</script>
