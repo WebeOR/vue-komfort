295b1181323b04a99fdd502ead891f5e0c55373e
AlexQuidditch <alexquidditch@gmail.com>
2018-09-29 22:22:16 +0300

    - мусор

--- src/App.vue
+++ src/App.vue
@@ -49,15 +49,4 @@
 
   @import './stylesheets/base.scss';
 
-  #nav {
-    padding: 30px;
-    a {
-      font-weight: bold;
-      color: #2c3e50;
-      &.router-link-exact-active {
-        color: #42b983;
-      }
-    }
-  }
-
 </style>
