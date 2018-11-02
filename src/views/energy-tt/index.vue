966490c9b8d7892e32d664ee20acb0352a55403a
AlexQuidditch <alexquidditch@gmail.com>
2018-11-02 14:22:13 +0300

    - энергия-тт

--- src/views/energy-tt/index.vue
+++ src/views/energy-tt/index.vue
@@ -1,255 +1,183 @@
 <template>
-  <main id="main" class="main">
+  <div class="main energy-tt">
 
-    <section class="intro">
-
-      <div class="intro__back-image-container">
-        <img src="assets/img/buderus/intro.jpg" alt="Buderus intro" class="intro__back-image">
-      </div>
-
-      <div class="intro__overlay">
-
-        <div class="container_flex">
-
-          <h1 class="intro__title">Твёрдотопливные котлы Энергия-ТТ</h1>
-
-          <span class="intro__subtitle">
-                    Выберите интересующий Вас вариант, и свяжитесь с менеджером, чтобы уточнить наличие и стоимость:
-                </span>
-
-          <ul class="intro__list">
-            <li><a href="#" data-roll="energy" class="intro__link" ripple-light>Энергия-ТТ</a></li>
-          </ul>
-
-          <span class="intro__additional-text">
-                	Котельная на твёрдом топливе может использоваться даже там, куда не подведены ни газопровод, ни линия электропередач, - твердотопливные котлы полностью автономны. Это преимущество твердотопливных отопительных котлов в сочетании с невысокой стоимостью котла, а также доступностью и дешевизной топлива, обусловливает их популярность в России
-                </span>
-
-          <span class="intro__list-title">Другие семейства твёрдотопливных котлов:</span>
-
-          <ul class="intro__list">
-            <li><a href="kentatsu" class="intro__link" ripple-light>Kentatsu Furst Vulkan</a></li>
-            <li><a href="termokraft" class="intro__link" ripple-light>Termokraft</a></li>
-            <li><a href="buderus" class="intro__link" ripple-light>Buderus</a></li>
-            <li><a href="zota" class="intro__link" ripple-light>Zota</a></li>
-            <li><a href="nmk" class="intro__link" ripple-light>НМК</a></li>
-          </ul>
-
-        </div>
-
-      </div>
-
+    <section class="product">
+      <h1 class="md-display-2">Твёрдотопливные котлы Энергия-ТТ</h1>
+      <p class="md-body-2">
+        Котельная на твёрдом топливе может использоваться даже там, куда не подведены ни газопровод, ни линия электропередач, - твердотопливные котлы полностью автономны. Это преимущество твердотопливных отопительных котлов в сочетании с невысокой стоимостью котла, а также доступностью и дешевизной топлива, обусловливает их популярность в России.
+      </p>
     </section>
 
+    <Energy-Template
+      v-for="line in lines" :key="line.id"
+      :Line="line" />
 
-    <section id="energy" class="product">
-
-      <div class="product__container">
-
-        <h2 class="product__name"></h2>
-
-        <div class="container_flex-row">
-
-          <div class="product__slider-container" flickity>
-
-            <div class="carousel-cell">
-              <img src="assets/img/energy-tt/energy.jpg" alt="Энергия-ТТ" class="product__image">
-            </div>
-
-            <div class="carousel-cell">
-              <img src="assets/img/energy-tt/energy-1.jpg" alt="Энергия-ТТ" class="product__image">
-            </div>
-
-            <div class="carousel-cell">
-              <img src="assets/img/energy-tt/energy-2.jpg" alt="Энергия-ТТ" class="product__image">
-            </div>
-
-          </div>
-
-          <div class="product__description-container">
-
-            <span class="product__characteristics-title">Характеристики:</span>
-
-            <ul class="product__characteristics-list">
-              <li class="product__characteristics-item">
-                Применяется для обогрева жилых и административных зданий, производственных помещений;
-              </li>
-              <li class="product__characteristics-item">
-                Горение топлива происходит в нижней части котла, оборудованной проточными колосниками;
-              </li>
-              <li class="product__characteristics-item">
-                Загрузка топлива значительного объема позволяет обеспечить его горение в течение значительного периода (3-14 суток, в зависимости от теплопотерь здания, качества топлива и объема бункера котла);
-              </li>
-              <li class="product__characteristics-item">
-                Загрузка топлива ручная, удаление шлака ручное;
-              </li>
-              <li class="product__characteristics-item">
-                Топливо: антрацит, каменный уголь, бурый уголь, угольные брикеты, дрова;
-              </li>
-              <li class="product__characteristics-item">
-                Длительность горения одной закладки: при максимальной мощности – 3-5 суток, средняя до 7 суток, в межсезонье до 14 суток;
-              </li>
-              <li class="product__characteristics-item">
-                Тип тяги: естественная;
-              </li>
-              <li class="product__characteristics-item">
-                Управление: электронное, контроллером управления работой наддува, насосами системы отопления;
-              </li>
-            </ul>
-          </div>
-
-          <div class="product__table-container" align="center">
-            <table style="border-collapse:collapse;table-layout:fixed" width="1093" border="0" cellspacing="0" cellpadding="0">
-              <colgroup>
-                <col width="122">
-                <col width="77">
-                <col width="101">
-                <col width="49">
-                <col width="101">
-                <col width="77">
-                <col width="78">
-                <col width="77">
-                <col width="95">
-                <col width="99">
-                <col width="111">
-                <col width="106">
-              </colgroup>
-              <tbody>
-                <tr height="80">
-                  <td>Характеристики</td>
-                  <td>Мах мощность, кВт</td>
-                  <td>Отапливаемая площадь, м&sup2;</td>
-                  <td>КПД, %</td>
-                  <td>Габариты, мм (ДхШхВ)</td>
-                  <td>Диаметр дымохода, мм</td>
-                  <td>Объем воды в котле, л</td>
-                  <td>Масса котла нетто, кг</td>
-                  <td>Макc вес загружаемого угля, кг</td>
-                  <td>Макс. рабочее избыточное давление, бар</td>
-                  <td>Мин./макс. темп. на выходе из котла, °С</td>
-                  <td>Стоимость
-                    <br>(без кожуха
-                    <br>/с кожухом)
-                  </td>
-                </tr>
-                <tr height="30">
-                  <td>Энергия ТТ 9 кВт</td>
-                  <td>9</td>
-                  <td>до 80</td>
-                  <td>70</td>
-                  <td>600х570х800</td>
-                  <td>150</td>
-                  <td>18</td>
-                  <td>80</td>
-                  <td>60</td>
-                  <td>2</td>
-                  <td>40/90</td>
-                  <td>49 000/59 000</td>
-                </tr>
-                <tr height="30">
-                  <td>Энергия ТТ 14 кВт</td>
-                  <td>14</td>
-                  <td>до 130</td>
-                  <td>70</td>
-                  <td>780х570х1350</td>
-                  <td>150</td>
-                  <td>27</td>
-                  <td>150</td>
-                  <td>100</td>
-                  <td>2</td>
-                  <td>40/90</td>
-                  <td>79 000/89 000</td>
-                </tr>
-                <tr height="30">
-                  <td>Энергия ТТ 18 кВт</td>
-                  <td>18</td>
-                  <td>до 180</td>
-                  <td>70</td>
-                  <td>990х700х1550</td>
-                  <td>150</td>
-                  <td>35</td>
-                  <td>190</td>
-                  <td>200</td>
-                  <td>2</td>
-                  <td>40/90</td>
-                  <td>89 000/101 000</td>
-                </tr>
-                <tr height="30">
-                  <td>Энергия ТТ 25 кВт</td>
-                  <td>25</td>
-                  <td>до 250</td>
-                  <td>70</td>
-                  <td>1400х770х1600</td>
-                  <td>150</td>
-                  <td>57</td>
-                  <td>290</td>
-                  <td>310</td>
-                  <td>2</td>
-                  <td>40/90</td>
-                  <td>99 000/112 000</td>
-                </tr>
-                <tr height="30">
-                  <td>Энергия ТТ 40 кВт</td>
-                  <td>40</td>
-                  <td>до 400</td>
-                  <td>70</td>
-                  <td>1000х770х1600</td>
-                  <td>150</td>
-                  <td>60</td>
-                  <td>320</td>
-                  <td>290</td>
-                  <td>2</td>
-                  <td>40/90</td>
-                  <td>109 000/122 000</td>
-                </tr>
-                <tr style="display:none" height="0">
-                  <td style="width:92pt" width="122"></td>
-                  <td style="width:58pt" width="77"></td>
-                  <td style="width:76pt" width="101"></td>
-                  <td style="width:37pt" width="49"></td>
-                  <td style="width:76pt" width="101"></td>
-                  <td style="width:58pt" width="77"></td>
-                  <td style="width:59pt" width="78"></td>
-                  <td style="width:58pt" width="77"></td>
-                  <td style="width:71pt" width="95"></td>
-                  <td style="width:74pt" width="99"></td>
-                  <td style="width:83pt" width="111"></td>
-                  <td style="width:80pt" width="106"></td>
-                </tr>
-              </tbody>
-            </table>
-
-          </div>
-
-        </div>
-
-      </div>
-
-    </section>
-
-    <div class="s-devider">
-      <div class="line"></div>
-    </div>
-
-    <div class="families">
-
-      <span class="families__list-title">Другие семейства твёрдотопливных котлов:</span>
-      <ul class="families__list">
-        <li><a href="kentatsu" class="families__link" ripple-light>Kentatsu Furst Vulkan</a></li>
-        <li><a href="termokraft" class="families__link" ripple-light>Termokraft</a></li>
-        <li><a href="buderus" class="families__link" ripple-light>Buderus</a></li>
-        <li><a href="zota" class="families__link" ripple-light>Zota</a></li>
-        <li><a href="nmk" class="families__link" ripple-light>НМК</a></li>
-      </ul>
-
-    </div>
-
-  </main>
+  </div>
 </template>
 
+<script>
+
+  import EnergyTemplate from './EnergyTemplate.vue';
+
+  export default {
+    name: 'Energy-TT',
+
+    components: { EnergyTemplate },
+
+    data: () => ({
+      lines: [
+
+        {
+          id: 'energy',
+          title: 'Энергия-ТТ',
+          image: null,
+          images: [
+            '/img/energy-tt/energy.jpg',
+            '/img/energy-tt/energy-1.jpg',
+            '/img/energy-tt/energy-2.jpg',
+          ],
+          price: null,
+          characteristics: [
+            'Применяется для обогрева жилых и административных зданий, производственных помещений;',
+            'Горение топлива происходит в нижней части котла, оборудованной проточными колосниками;',
+            'Загрузка топлива значительного объема позволяет обеспечить его горение в течение значительного периода (3-14 суток, в зависимости от теплопотерь здания, качества топлива и объема бункера котла);',
+            'Загрузка топлива ручная, удаление шлака ручное;',
+            'Топливо: антрацит, каменный уголь, бурый уголь, угольные брикеты, дрова;',
+            'Длительность горения одной закладки: при максимальной мощности – 3-5 суток, средняя до 7 суток, в межсезонье до 14 суток;',
+            'Тип тяги: естественная;',
+            'Управление: электронное, контроллером управления работой наддува, насосами системы отопления;',
+          ],
+          docs: null,
+          descriptions: [
+            ''
+          ],
+          table: {
+            head: [
+              'Характеристики',
+              'Мах мощность, кВт',
+              'Отапливаемая площадь, м2',
+              'КПД, %',
+              'Габариты, мм (ДхШхВ)',
+              'Диаметр дымохода, мм',
+              'Объем воды в котле, л',
+              'Масса котла нетто, кг',
+              'Макc вес загружаемого угля, кг',
+              'Макс. рабочее избыточное давление, бар',
+              'Мин./макс. темп. на выходе из котла, °С',
+              'Стоимость (без кожуха/с кожухом)',
+            ],
+            body: [
+              [
+                'Энергия ТТ 9 кВт',
+                '9',
+                'до 80',
+                '70',
+                '600х570х800',
+                '150',
+                '18',
+                '80',
+                '60',
+                '2',
+                '40/90',
+                '49 000/59 000',
+              ],
+              [
+                'Энергия ТТ 14 кВт',
+                '14',
+                'до 130',
+                '70',
+                '780х570х1350',
+                '150',
+                '27',
+                '150',
+                '100',
+                '2',
+                '40/90',
+                '79 000/89 000',
+              ],
+              [
+                'Энергия ТТ 18 кВт',
+                '18',
+                'до 180',
+                '70',
+                '990х700х1550',
+                '150',
+                '35',
+                '190',
+                '200',
+                '2',
+                '40/90',
+                '89 000/101 000',
+              ],
+              [
+                'Энергия ТТ 25 кВт',
+                '25',
+                'до 250',
+                '70',
+                '1400х770х1600',
+                '150',
+                '57',
+                '290',
+                '310',
+                '2',
+                '40/90',
+                '99 000/112 000',
+              ],
+              [
+                'Энергия ТТ 40 кВт',
+                '40',
+                'до 400',
+                '70',
+                '1000х770х1600',
+                '150',
+                '60',
+                '320',
+                '290',
+                '2',
+                '40/90',
+                '109 000/122 000',
+              ],
+            ]
+          }
+        },
+
+      ]
+    }),
+
+    computed: {
+      toolbar () {
+        return this.lines.map( ({ id , title }) => {
+          return {
+            id,
+            title: title.includes('Kentatsu') ? title.slice(9) : title
+          };
+        });
+      }
+    },
+
+    created () {
+      this.$emit( 'set-toolbar' , this.toolbar );
+    }
+  };
+
+</script>
 
 <style lang="scss">
-    
- @import '../../stylesheets/energy-tt.scss';
- 
+
+  .main.energy-tt {
+    .md-card {
+      overflow: hidden;
+    }
+    .md-table-head {
+      vertical-align: top;
+    }
+    .md-table-head-container,
+    .md-table-head-label {
+      height: initial;
+      overflow: unset;
+      text-overflow: unset;
+      white-space: unset;
+    }
+  }
+
 </style>
