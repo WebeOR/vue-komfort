9e4b773387c68c2d4875182f4d38d42593335260
AlexQuidditch <alexquidditch@gmail.com>
2019-02-28 01:38:07 +0300

    - доработан слайдер

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,15 +1,15 @@
 <template>
   <swiper
+    v-if="slides && slides.length"
     :options="swiperOption"
-    class="md-layout-item md-size-50">
+    class="md-layout-item md-size-50 md-small-size-100">
     <swiper-slide
       v-for="( slide , index ) in slides"
       :key="index">
-      <img
-        :key="index"
+      <Preview
         :src="slide"
         :alt="title"
-        style="object-fit:contain" />
+        />
     </swiper-slide>
     <div class="swiper-pagination" slot="pagination"></div>
     <div class="swiper-button-prev" slot="button-prev"></div>
@@ -19,43 +19,49 @@
 
 <script>
 
-  import 'swiper/dist/css/swiper.css';
+import 'swiper/dist/css/swiper.css';
 
-  import { swiper, swiperSlide } from 'vue-awesome-swiper';
+import { swiper, swiperSlide } from 'vue-awesome-swiper';
 
-  export default {
-    name: 'Template-Slider',
-    components: {
-      swiper,
-      swiperSlide
+import Preview from './Preview.vue';
+
+export default {
+  name: 'Slider',
+  components: {
+    swiper,
+    swiperSlide,
+    Preview,
+  },
+
+  props: {
+    slides: {
+      type: Array,
+      required: true,
     },
+    title: {
+      type: String,
+      default: '',
+    },
+    noPreview: {
+      type: Boolean,
+      default: false,
+    },
+  },
 
-    props: {
-      'title': {
-        type: String,
-        default: '',
+  data: () => ({
+    swiperOption: {
+      grabCursor: true,
+      loop: false,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
       },
-      'slides': {
-        type: Array,
-        required: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
       },
     },
-
-    data: () => ({
-      swiperOption: {
-        grabCursor: true,
-        loop: true,
-        navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev'
-        },
-        pagination: {
-          el: '.swiper-pagination',
-          clickable: true
-        },
-      },
-    }),
-
-  };
+  }),
+};
 
 </script>
